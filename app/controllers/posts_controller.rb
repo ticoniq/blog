0334cad27b1c6b8c94071cc90efc34111ba403a4
ticoniq_ <okolietochi@gmail>
2023-09-30 02:24:34 +0100

    Blog app - Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,40 @@ class PostsController < ApplicationController
     end
   end
 
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_path(current_user), notice: 'Post was successfully created.'
+    else
+      puts @post.errors.full_messages
+      render :new
+    end
+  end
+
+  def like
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+
+    @like = @post.likes.new
+    @like.author = current_user
+
+    if @like.save
+      flash[:notice] = 'Post liked successfully.'
+    else
+      flash[:alert] = 'Failed to like the post.'
+    end
+
+    redirect_to user_post_path(@user, @post)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+
   private
 
   def set_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,12 @@ require 'faker'
     name: Faker::Name.name,
     photo: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
     bio: Faker::Lorem.paragraphs(number: 4).join("\n\n"),
-    posts_counter: rand(5..15) # Random post count for each user (between 5 and 15)
+    posts_counter: rand(5..15) 
   )
 
   user.posts_counter.times do
     Post.create(
-      author: user, # Assign the user as the author
+      author: user, 
       title: Faker::Lorem.sentence,
       text: Faker::Lorem.paragraphs(number: 4).join("\n\n"),
       likes_counter: rand(0..50),
@@ -27,8 +27,8 @@ require 'faker'
 
   50.times do
   Comment.create(
-    user_id: User.order('RANDOM()').first.id, # Assign a random user as the commenter
-    post_id: Post.order('RANDOM()').first.id, # Assign a random post as the commented post
+    user_id: User.order('RANDOM()').first.id,
+    post_id: Post.order('RANDOM()').first.id, 
     text: Faker::Lorem.sentence
   )
 end
